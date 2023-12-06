ebc2f3ee7e76d79fb5daa304409d510c6e191821
abeedev17 <as.asuperstar05@gmail.com>
2023-12-06 10:37:21 -0300

    project setup

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import "./styles/index.scss";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Olá Mundo</h1>
+      <p>Meu primeiro APP Next.js & Sass!</p>
+    </main>
+  );
+}
